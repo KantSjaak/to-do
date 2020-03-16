22d7bc2932e496ef1e14fc045feae921f43c7757
sjaak <kantsjaak@gmail.com>
2020-03-16 19:57:38 +0100

    updated it but needed it on a different device

--- actions/viewlist.php
+++ actions/viewlist.php
@@ -11,7 +11,7 @@ $pass = "";
 try {
     $conn = new PDO('mysql:host=localhost;dbname=todobase', $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT `listName` FROM `listtable` ORDER BY `id` ASC");
+    $stmt = $conn->prepare("SELECT `id`, `listName` FROM `listtable` ORDER BY `id` ASC");
     $stmt->execute();
 
     // set the resulting array to associative
@@ -20,7 +20,29 @@ try {
     foreach($stmt->fetchAll() as $k=>$v) {
         echo "<div class='listItem'>". $v["listName"]. "<br></div>";
     }
+    //edit function and add edit text as onclicks
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
+}
+
+edit(11, "work list");
+
+function edit($id, $name)
+{
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=todobase', "root", "");
+
+        $stmt = $dbh->prepare("UPDATE `listtable` SET `listName` = (:newvalue) WHERE `id` = (:idvalue)");
+        $stmt->bindParam(':newvalue', $name);
+        $stmt->bindParam(':idvalue', $id);
+
+        $stmt->execute();
+
+        $dbh = null;
+        echo "<p id='succesMessage'>changed list " . $name . " in the database</p>";
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
 }
